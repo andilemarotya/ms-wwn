15ef7fb65ae02942068f519a5fee21203e72f8d8
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-06-11 13:23:22 +0200

    WebView

--- ms_wwnews/lib/views/article_view.dart
+++ ms_wwnews/lib/views/article_view.dart
@@ -7,69 +7,74 @@ import '../menu.dart';
 class ArticleView extends StatefulWidget {
   final String blogUrl;
 
-  ArticleView({required this.blogUrl,super.key});
-  WebViewController controlla = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(const Color(0x00000000))
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-      ),
-    )
-    ..loadRequest(Uri.parse(blogUrl));
+  ArticleView({required this.blogUrl, super.key});
+  
 
   @override
   State<ArticleView> createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
-
   //Adding page load events
   @override
   var loadingPercentage = 0;
-  
+  late WebViewController controlla;
 
   @override
   void initState() {
     super.initState();
+    controlla = WebViewController()
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+      ))
+      ..loadRequest(
+        Uri.parse(widget.blogUrl),
+      );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar (
-         title: Row(
+      appBar: AppBar(
+        title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Text("MS ", style: TextStyle(color: Colors.black)),
             Text("WWNews", style: TextStyle(color: Colors.blue))
           ],
         ),
         actions: [
+          Opacity(
+            opacity: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.save, color: Colors.black38)),
+          ),   //This opacity is to center the title since there's back button
           
-          NavigationControls(controller: widget.controlla),
-          Menu(controller: widget.controlla),
         ],
         centerTitle: true,
         elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black87),
+        backgroundColor: Colors.grey[100],
       ),
 
       body: WebViewWidget(
-        controller: widget.controlla,
+        controller: controlla,
       ),
-        
-      
     );
   }
 }
